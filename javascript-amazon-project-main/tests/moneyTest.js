68621074851f63b23aa438bd4d2070649494413d
jiteshshankhpal <jshankhpal41@gmail.com>
2025-10-14 13:00:49 +0530

    Testing framework

--- javascript-amazon-project-main/tests/moneyTest.js
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -1,22 +1,22 @@
-import { formatCurrency } from '../scripts/utils/money.js'
+import {formatCurrency} from '../../javascript-amazon-project-main/scripts/utils/money.js'
 
 console.log('Test suite: format curreny')
 console.log('Converts cents into dollars')
-if(formatCurrency('2095') === '20.95') {
+if(formatCurrency(2095) === '20.95') {
     console.log('Passed');
 } else {
     console.log('Failed');
 }
 
 console.log('works with 0')
-if (formatCurrency('0') === '0.00') {
+if (formatCurrency(0) === '0.00') {
     console.log('Passed');
 } else {
     console.log('Failed');
 }
 
 console.log('convets to nearest cent')
-if (formatCurrency('2000.5') === '20.01') {
+if (formatCurrency(2000.5) === '20.01') {
     console.log('Passed');
 } else {
     console.log('Failed');
